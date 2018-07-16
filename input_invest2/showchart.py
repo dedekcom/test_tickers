1c6ed7351104f971a3c4c42d9ebe4a811ffc2f99
DominikDa <dominikda@advaoptical.com>
2018-07-16 17:20:40 +0200

    create dirs

--- input_invest2/showchart.py
+++ input_invest2/showchart.py
@@ -6,26 +6,38 @@ import matplotlib.pyplot as plt
 
 if len(sys.argv) < 2:
 	print ('not enough input argument')
-	print ('usage: python ' + sys.argv[0] + ' <input.csv>')
+	print ('usage: python ' + sys.argv[0] + ' <input.csv>|<input.mst>')
 	sys.exit(0)
 
 inputfile = sys.argv[1]
+filetype = inputfile[-3:]
 
 try:
 	datareg = re.compile("[0-9]+-[0-9]+[0-9]+")
+	tickermst = re.compile("[A-Z]+")
 	ticker = open(inputfile)
 	yvalues = []
 	xvalues = []
 	xlabels = []
 	xval = 0
-	for line in ticker.readlines():
-		x = line.rstrip().split(',')
-		if len(x) > 0 and datareg.match(x[0]):
-			# Data,Otwarcie,Najwyzszy,Najnizszy,Zamkniecie,Wolumen
-			yvalues.append(float(x[4]))
-			xvalues.append(xval)
-			xlabels.append(x[0])
-			xval = xval + 1
+	if filetype == "csv":
+		for line in ticker.readlines():
+			x = line.rstrip().split(',')
+			if len(x) > 0 and datareg.match(x[0]):
+				# Data,Otwarcie,Najwyzszy,Najnizszy,Zamkniecie,Wolumen
+				yvalues.append(float(x[4]))
+				xvalues.append(xval)
+				xlabels.append(x[0])
+				xval = xval + 1
+	elif filetype == "mst":
+		for line in ticker.readlines():
+			x = line.rstrip().split(',')
+			if len(x) > 0 and tickermst.match(x[0]):
+				#<TICKER>,<DTYYYYMMDD>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>
+				yvalues.append(float(x[5]))
+				xvalues.append(xval)
+				xlabels.append(x[1])
+				xval = xval + 1
 	ticker.close()
 
 	plt.plot(xvalues, yvalues, label='linear')
